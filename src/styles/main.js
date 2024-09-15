e08f27d43a65415ff127c524f5236d238479b5e1
bcortez-gomes <160198522+bcortez-gomes@users.noreply.github.com>
2024-09-15 00:01:05 -0300

    ajustes no mobile

--- src/styles/main.js
+++ src/styles/main.js
@@ -1,6 +1,22 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
+
+    //ajuste do cabeçalho com delimitador
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosHeader();
+        } else {
+            exibeElementoHeader();
+        }
+    })
     
+    //seção de atrações, programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -11,7 +27,25 @@ document.addEventListener('DOMContentLoaded', function(){
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    //seção FAQ
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+        
+    }
 })
+
+function ocultaElementosHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+    
+}
+
+function exibeElementoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+    
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
@@ -27,4 +61,12 @@ function escondeTodasAsAbas() {
     for (let i = 0; i < tabsContainer.length; i++) {
         tabsContainer[i].classList.remove('shows__list--is-active');
     }
+}
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const ElementoPai = elemento.target.parentNode;
+
+    ElementoPai.classList.toggle(classe);
+
 }
